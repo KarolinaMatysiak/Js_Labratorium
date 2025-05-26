4d5408f32eb208a9ed9c922d0f332670ac2a90c0
karolincia <karolina.matysiak@microsoft.wsei.edu.pl>
2025-05-26 16:43:27 +0200

    add pause button

--- script.js
+++ script.js
@@ -2,15 +2,20 @@
 const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slide');
 const dots = document.querySelectorAll('.dot');
+const pauseButton = document.querySelector('.pause-button');
 let currentSlide = 0;
 const totalSlides = slides.length;
 
 
+let sliderInterval;
+let isPaused = false;
+
+
 function showSlide(slideNumber) {
-   
+
     slider.style.transform = 'translateX(-' + (slideNumber * 100) + '%)';
     
-   
+
     for(let i = 0; i < dots.length; i++) {
         if(i === slideNumber) {
             dots[i].className = 'dot active';
@@ -19,34 +24,53 @@ function showSlide(slideNumber) {
         }
     }
     
-   
+
     currentSlide = slideNumber;
 }
 
 
 function nextSlide() {
     if(currentSlide >= totalSlides - 1) {
-        
+
         showSlide(0);
     } else {
-      
+     
         showSlide(currentSlide + 1);
     }
 }
 
-
 function prevSlide() {
     if(currentSlide <= 0) {
-  
         showSlide(totalSlides - 1);
     } else {
-       
+    
         showSlide(currentSlide - 1);
     }
 }
 
+function autoSlide() {
+    isPaused = !isPaused;
 
-setInterval(nextSlide, 3000);
+    if (isPaused) {
+        pauseButton.className = 'pause-button paused';
+ 
+        clearInterval(sliderInterval);
+    } else {
+        pauseButton.className = 'pause-button';
+ 
+        startSliderInterval();
+    }
+}
 
 
-showSlide(0); 
+function startSliderInterval() {
+
+    if (sliderInterval) {
+        clearInterval(sliderInterval);
+    }
+
+    sliderInterval = setInterval(nextSlide, 3000);
+}
+
+showSlide(0);
+startSliderInterval(); 
